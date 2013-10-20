ccbb3b4a93faa01c4eff7c34ee502a7eb99942dc
Konstantin Litvinenko <to.darkangel@gmail.com>
2013-10-20 09:59:50 +0300

    gc solved

---
+++ gc/gc.hs
@@ -0,0 +1,25 @@
+import System.Environment(getArgs)
+import Data.List(maximumBy, span, foldl')
+
+main = do
+    s <- getArgs >>= read_file
+    let (s_id, gc_content) = maximumBy (\(_, lhs) (_, rhs) -> compare lhs rhs) $ to_gc_content (parse_fasta s)
+    putStrLn s_id
+    print gc_content
+  where
+    read_file (filename:_) = readFile filename >>= return   
+    read_file _ = error "one argument required"
+
+-- simple parser that doesn't check any errors
+parse_fasta content = go $ lines content 
+  where 
+    go [] = []
+    go (id_line:xs) = let (body, xs') = break (\(x:_) -> x == '>') xs
+                      in make_one (tail id_line) body : go xs'
+    make_one id_line body = (head $ words id_line, concat body)
+
+to_gc_content = map $ \(sid, body) -> (sid, calc_gc body)
+  where
+    calc_gc body = let (l, gc) = foldl' (\(l, acc) x -> if x == 'C' || x == 'G' then (l + 1, acc + 1) else (l + 1, acc)) (0, 0) body
+                   in fromIntegral gc / fromIntegral l * (100.0 :: Double)
+    
