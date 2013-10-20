8fbd5860f9836e6df5d0387351a67b28911a183d
Konstantin Litvinenko <to.darkangel@gmail.com>
2013-10-20 16:05:06 +0300

    prtm solved

---
+++ prtm/prtm.hs
@@ -0,0 +1,35 @@
+import System.Environment(getArgs)
+import Data.List(foldl')
+import qualified Data.HashMap.Strict as H
+import Data.Maybe(fromJust)
+
+main = do
+    s <- getArgs >>= read_line
+    print $ mass s
+  where
+    read_line (filename:_) = readFile filename >>= return . head . lines 
+    read_line _ = error "one argument required"
+
+get_mass x = fromJust $ H.lookup x $ H.fromList [
+    ('A',   71.03711),
+    ('C',   103.00919),
+    ('D',   115.02694),
+    ('E',   129.04259),
+    ('F',   147.06841),
+    ('G',   57.02146),
+    ('H',   137.05891),
+    ('I',   113.08406),
+    ('K',   128.09496),
+    ('L',   113.08406),
+    ('M',   131.04049),
+    ('N',   114.04293),
+    ('P',   97.05276),
+    ('Q',   128.05858),
+    ('R',   156.10111),
+    ('S',   87.03203),
+    ('T',   101.04768),
+    ('V',   99.06841),
+    ('W',   186.07931),
+    ('Y',   163.06333) 
+           ]
+mass = foldl' (\acc x -> acc + get_mass x) 0.0 
