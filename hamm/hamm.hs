5c1cb3e953239d23f42b2f611ae3b68e7c1a0f1e
Konstantin Litvinenko <to.darkangel@gmail.com>
2013-10-20 14:36:15 +0300

    hamm solved

---
+++ hamm/hamm.hs
@@ -0,0 +1,13 @@
+import System.Environment(getArgs)
+import Data.List(foldl')
+
+main = do
+    (s1:s2:_) <- getArgs >>= read_lines
+    print $ hamm s1 s2
+  where
+    read_lines (filename:_) = readFile filename >>= return . lines 
+    read_lines _ = error "one argument required"
+
+hamm s1 s2 = foldl' hf 0 $ zip s1 s2
+  where
+    hf acc (x, y) = if x == y then acc else acc + 1
