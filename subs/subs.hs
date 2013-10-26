d7455d7cf76f716bb25d859873c2f0b34b1cf798
Konstantin Litvinenko <to.darkangel@gmail.com>
2013-10-26 09:40:56 +0300

    subs solved

---
+++ subs/subs.hs
@@ -0,0 +1,20 @@
+import System.Environment(getArgs)
+import Data.List(isPrefixOf)
+import Debug.Trace
+
+main = do
+    (s1:s2:_) <- getArgs >>= read_lines
+    mapM_ (\x -> (putStr $ show x) >> putStr " ") $ subs s1 s2
+    putStrLn ""
+  where
+    read_lines (filename:_) = readFile filename >>= return . lines 
+    read_lines _ = error "one argument required"
+
+subs s t = go s 1 
+  where 
+    go [] _ = []
+    go x@(s':s'') sp = if isPrefixOf t x 
+                           then sp : go s'' (sp + 1)
+                           else go s'' (sp + 1)
+
+
